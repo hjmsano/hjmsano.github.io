9f1a3a1b2b1d77b5a1ca8ee9ceac208dc724b7a1
hjmsano <hjmsano@users.noreply.github.com>
2020-04-25 15:20:00 +0000

    deploy: 7fdabf027a0b00939207176a846d6d7b5cd85128

---
+++ components/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <section class="hero is-dark ">
+        <div class="hero-body">
+            <h1 class="title">
+                Hajime Sano on GitHub
+            </h1>
+            <h2 class="subtitle">
+                Here is my portfolio that introduce my own projects and repositories contributed to.
+            </h2>
+        </div>
+    </section>
+</template>
